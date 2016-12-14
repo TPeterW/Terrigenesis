8dcded684bdd6387d19037b16727aff6f375faf5
TPeterW <peterwangtao0@hotmail.com>
2016-12-14 01:36:35 -0500

    This is what I want to do

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fileserver/server"
+)
+
+func main() {
+	server.StartServer()
+}
---
+++ utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+/*
+Session representing an open session between client and server
+*/
+type Session struct {
+	ID       string
+	LastUsed string
+}
