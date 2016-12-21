1f599f2d754d3b5b30b6aa4cdbfe5688b1dc7b9f
TPeterW <peterwangtao0@hotmail.com>
2016-12-21 03:06:07 -0500

    Added snake game for root and exception pages

--- fileserver/server/server.go
+++ fileserver/server/server.go
@@ -37,6 +37,12 @@ func StartServer() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("\n", r)
 
+	// root page, render snakey
+	if len(r.URL.Path) <= 1 {
+		renderSnake(w)
+		return
+	}
+
 	splited := strings.Split(r.URL.Path[1:], "/")
 
 	if r.Method == "GET" {
@@ -67,6 +73,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGet(w http.ResponseWriter, r *http.Request, request string, sessions []utils.Session) []utils.Session {
+	defer r.Body.Close()
+
 	if r.URL.Query()["Token"] == nil {
 		handlers.IllegalArgumentsError(w)
 		return sessions
@@ -90,7 +98,7 @@ func handleGet(w http.ResponseWriter, r *http.Request, request string, sessions 
 		fmt.Println(">>> Download File")
 
 	default:
-		// TODO: render a snake game
+		renderSnake(w)
 	}
 
 	// TODO: replace original session with current one
@@ -141,12 +149,18 @@ func handlePost(w http.ResponseWriter, r *http.Request, request string, sessions
 		fmt.Println(">>> Move File")
 
 	default:
-		// TODO: render a snake game
+		renderSnake(w)
 	}
 
 	// TODO: replace original session with current one
 
 	return sessions
+}
+
+// render a snake game
+func renderSnake(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Snakey!!!</title><canvas id=\"canvas\" width=\"400\" height=\"400\"></canvas></head><body><script>window.onkeydown=((ctx,snake,food,direction,move,draw)=>((loop,newFood,timer)=>Array.from({length:400}).forEach((_e,i)=>draw(ctx,i,\"black\"))||(timer=setInterval(()=>loop(newFood)||clearInterval(timer)||console.log(timer)||alert('Game Over'),200))&&(e=>direction=snake[1]-snake[0]==(move=[-1,-20,1,20][(e||event).keyCode-37]||direction)?direction:move))((newFood)=>snake.unshift(move=snake[0]+direction)&&snake.indexOf(move,1)>0||move<0||move>399||direction==1&&move%20==0||direction==-1&&move%20==19?false:(draw(ctx,move,\"green\")||move==food?newFood()&draw(ctx,food,\"red\"):draw(ctx,snake.pop(),\"Black\"))!==[],()=>Array.from({length:8000}).some(e=>snake.indexOf(food=~~(Math.random()*400))===-1)))(document.getElementById('canvas').getContext('2d'),[42,41],43,1,null,(ctx,node,color)=>(ctx.fillStyle=color)&ctx.fillRect(node%20*20+1,~~(node/20)*20+1,18,18));</script></body></html>")
 }
 
 func handleInterrupt() {
